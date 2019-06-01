580e572b75e35ee7c97eb71436bc2205038f05dd
Babak RezaeeDaryakenari <24599548+babakrezaee@users.noreply.github.com>
2019-06-01 21:23:07 +0300

    Update doCVlogit.R

--- Functions/doCVlogit.R
+++ Functions/doCVlogit.R
@@ -1,16 +1,16 @@
 
 print("This function recieves 2 elements: data and formula; Set the inputs accordingly")
 
-doCVlogit<-function(data,formula,k){
-  k=k
+doCVlogit<-function(data,formula){
+
   #Randomly shuffle the data
   data<-data[sample(nrow(data)),]
   #Create k equally size folds
-  folds <- cut(seq(1,nrow(data)),breaks=k,labels=FALSE)
+  folds <- cut(seq(1,nrow(data)),breaks=10,labels=FALSE)
   
   fitted_values=rep(0,nrow(data))
   #Perform k fold cross validation
-  for(i in 1:k){
+  for(i in 1:10){
     #Segement your data by fold using the which() function 
     testIndexes <- which(folds==i,arr.ind=TRUE)
     testData <- data[testIndexes, ]
@@ -23,5 +23,4 @@ doCVlogit<-function(data,formula,k){
   return(fitted_values)
 }
 
-results=doCVlogit(cd_1,as.formula(Violent1~.))
 
