1838f2d719baaea91f80454c96366ba0dade1142
Babak RezaeeDaryakenari <24599548+babakrezaee@users.noreply.github.com>
2019-06-02 16:06:31 +0300

    Update doCVlogit.R

--- Functions/doCVlogit.R
+++ Functions/doCVlogit.R
@@ -4,8 +4,8 @@ print("This function recieves 3 elements: formula, data, and k (number of folds;
 doCVlogit<-function(Formula, Data, k=10){
   
   #a little error checking
-  if(!(is.data.frame(Data))) {cat('error in docv: "Data" is not a  data frame\n'); return(0)}
-  if(!(is.formula(formula))) {cat('error in docv: "formula" is not a formula\n'); return(0)}
+  if(!(is.data.frame(Data))) {cat('error in docv: "Data" is not a  data frame type\n'); return(0)}
+  if(!(is.formula(Formula))) {cat('error in docv: "Formula" is not a formula type\n'); return(0)}
   is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
   if(!(is.wholenumber(k) | k==1)) {cat('error in docv: k is not an integer larger than 1\n'); return(0)}
 
@@ -21,7 +21,7 @@ doCVlogit<-function(Formula, Data, k=10){
     testIndexes <- which(folds==i,arr.ind=TRUE)
     testData <- Data[testIndexes, ]
     trainData <- Data[-testIndexes, ]
-    fm=glm(formula,Data=trainData,family = "binomial")
+    fm=glm(Formula,Data=trainData,family = "binomial")
     fitted_values[as.numeric(rownames(testData))]<-predict(fm, testData,, type="response")
     
   }
