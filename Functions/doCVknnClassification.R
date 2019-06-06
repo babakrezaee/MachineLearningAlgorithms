2d53bf62af8adef0715f1a9694d7ddb5f1e182ce
Babak RezaeeDaryakenari <24599548+babakrezaee@users.noreply.github.com>
2019-06-06 18:45:20 +0300

    Update doCVknnClassification.R

--- Functions/doCVknnClassification.R
+++ Functions/doCVknnClassification.R
@@ -6,9 +6,9 @@
 ############################################################
 ############################################################
 
-print("This function recieves 3 elements: formula, data, number of folds(default is 10),and k of KNN (default is 20); Set the inputs accordingly")
+print("This function recieves 3 elements: formula, data, number of folds(default is 10), k of KNN (default is 20), and kernel (default is "rectangular") ; Set the inputs accordingly")
 
-doCVknnClassification<-function(formula, Data, nfolds=10,K=20){
+doCVknnClassification<-function(formula, Data, nfolds=10,K=20, kernel="rectangular"){
   
   #a little error checking
   if(!(is.data.frame(Data))) {cat('error in docv: "Data" is not a  data frame type\n'); return(0)}
@@ -30,7 +30,7 @@ doCVknnClassification<-function(formula, Data, nfolds=10,K=20){
     testIndexes <- which(folds==i,arr.ind=TRUE)
     testData <- Data[testIndexes, ]
     trainData <- Data[-testIndexes, ]
-    fm<-kknn(formula, train=trainData, test=testData, k=K, kernel="rectangular")
+    fm<-kknn(formula, train=trainData, test=testData, k=K, kernel=KKNkernel)
     fitted_values[as.numeric(rownames(testData))]<-fm$prob[,2]
   }
   
